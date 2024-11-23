97c18d3af2d941f79700e3e89b697d45df8bf4ca
MHLEE12 <coolmh12@gmail.com>
2024-11-23 22:17:54 +0900

    241123 backjun - 2557, 1008
    backjun	문제 2557, 1008

---
+++ src/step01/solution_1008.java
@@ -0,0 +1,15 @@
+package step01;
+
+import java.util.Scanner;
+
+public class solution_1008 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        double firstNum = s.nextDouble();
+        double secondNum = s.nextDouble();
+
+        System.out.print(firstNum / secondNum);
+
+    }
+}
---
+++ src/step01/solution_2557.java
@@ -0,0 +1,8 @@
+package step01;
+
+public class solution_2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
